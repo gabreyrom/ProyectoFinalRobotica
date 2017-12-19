327b951dafca7a922aeb2709984cfbfccb4c5eb3
gabreyrom <gabo.reyno@gmail.com>
2017-12-18 18:22:12 -0600

    Actualizacion del codigo para calcular el estado del robot

--- histograma.cpp
+++ histograma.cpp
@@ -1,75 +1,137 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Pose2D.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Int32MultiArray.h>
 #include <stdlib.h> 
 #include <iostream>
 #include <math.h>
 
-//variables recibidas
-double x, y, theta, xd, yd, thetad, L, hz, e, a, deltax, deltay, vectx, vecty;
+//variables de estado
+double estado[8]={0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125};
+double desconocido[8]={0.015,0.015,0.015,0.015,0.015,0.015,0.015,0.895};
+double derecha[8]={0.015,0.015,0.015,0.015,0.2,0.225,0.5,0.015};
+double centro[8]={0.015,0.015,0.05,0.825,0.05,0.015,0.015,0.015};
+double centroDer[8]={0.015,0.015,0.015,0.1,0.725,0.1,0.015,0.015};
+double izquierda[8]={0.5,0.225,0.2,0.015,0.015,0.015,0.015,0.015};
+double centroIzq[8]={0.015,0.1,0.725,0.1,0.015,0.015,0.015,0.015};
+double centrado[8]={0.015,0.015,0.25,0.425,0.25,0.015,0.015,0.015};
+
 //variables internas de control
-bool flag1,flag2;
-//variable de signo
-int sign;
+bool line_left,line_right,line_center;
+
 
 // callback para leer la posicion del movil
-void poseMessageReceived (const std_msgs::Int32MultiArray& msg1){
-	x=msg1.x;
-	y=msg1.y;
-	theta=msg1.theta;
-	flag1=true;
+void lineLeft (const std_msgs::Int32MultiArray& msg1){
+	line_left=true;
 }
+
+void lineCenter (const std_msgs::Int32MultiArray& msg2){
+	line_center=true;
+}
+
+void lineRight (const std_msgs::Int32MultiArray& msg3){
+	line_right=true;
+}
+
+void actualizaEst (double a[], double b[]){
+	int nConv=8;
+	int i,j;
+	double suma=0.0;
+	double edoAct[8]={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+
+	for(i=0;i<8;i=i+1){
+		edoAct[i]=a[i]*b[i];
+		suma=suma+edoAct[i];
+	}
+
+	for(j=0;j<8;j=j+1){
+		edoAct[j]=edoAct[j]/suma;
+		estado[j]=edoAct[j];
+	}
+
+
+} 
 
 
 int main (int argc, char **argv){
-	//variables internas
-	double rho, alpha, beta, krho, kalpha, kbeta, w, error;	
-	//variables de publicacion
-	double v, gamma;
 
-	flag1=false;
-	flag2=false;
+	line_left=true;
+	line_right=true;
+	line_center=true;
 	
 	// Inicializa ROS system y crea un nodo.
 	ros::init(argc, argv, "robot_controller");
 	ros::NodeHandle nh ;
 	// Crea un objeto publicador .
-	ros::Publisher pubV=nh.advertise<std_msgs::Float32>("/AutoNOMOS_mini/manual_control/velocity",1000);
+	ros::Publisher pubEst=nh.advertise<std_msgs::Float32MultiArray>("/estadoEstimado",1000);
 	// Crea un objeto suscriptor
-	ros::Subscriber subPCentro = nh.subscribe("/points/center", 1000, &poseMessageReceived);
-	ros::Subscriber subPDerecha = nh.subscribe("/points/right", 1000, &poseMessageReceived);
-	ros::Subscriber subPIzquierda = nh.subscribe("/points/left", 1000, &poseDMessageReceived);
+	ros::Subscriber subPCentro = nh.subscribe("/points/center", 1000, &lineCenter);
+	ros::Subscriber subPDerecha = nh.subscribe("/points/right", 1000, &lineRight);
+	ros::Subscriber subPIzquierda = nh.subscribe("/points/left", 1000, &lineLeft);
 	//Crea el objeto mensaje
-	std_msgs::Float32 msgSteering;
-	std_msgs::Float32 msgVelocity;
+	std_msgs::Float32MultiArray array;
 
-
-	//Obtener informacion del usuario
-	ROS_INFO_STREAM("Indique los Hertz");
-	std::cin >> hz;
 
 	// Ciclo a hz Hz
-	ros::Rate rate (hz);
-	
-	//Ángulos alpha y beta se calculan en radianes
-	//STEERING ESTA EN GRADOS
+	ros::Rate rate (3);
 
-	//Ciclo principal
-    	while (ros::ok()) {
-		if (flag1 && flag2){
-			//Calculos para pose deseada
-			ROS_INFO_STREAM("Velocity: "<< v<< ", Gamma: " << gamma);
-			
-			flag1=false;
-		}else{
-			ROS_INFO_STREAM("Velocity: 0" << ", Gamma: 0");
+	while (ros::ok()) {
 
+		if (line_left && line_center && line_right)
+		{
+			actualizaEst(estado,centrado);
+			ROS_INFO_STREAM("Centrado");
 		}
-		
+		else if (line_left && line_center && !line_right)
+		{
+			actualizaEst(estado,centroIzq);
+			ROS_INFO_STREAM("CentroIzq");
+		}
+		else if (line_left && !line_center && line_right)
+		{
+			actualizaEst(estado,centro);
+			ROS_INFO_STREAM("Centro");
+		}
+		else if (line_left && !line_center && !line_right)
+		{
+			actualizaEst(estado,izquierda);
+			ROS_INFO_STREAM("Izquierda");
+		}
+		else if (!line_left && line_center && line_right)
+		{
+			actualizaEst(estado,centroDer);
+			ROS_INFO_STREAM("CentroDer");
+		}
+		else if (!line_left && line_center && !line_right)
+		{
+			actualizaEst(estado,centro);
+			ROS_INFO_STREAM("Centro");
+		}
+		else if (!line_left && !line_center && line_right)
+		{
+			actualizaEst(estado,derecha);
+			ROS_INFO_STREAM("Derecha");
+		}
+		else{
+			actualizaEst(estado,desconocido);
+			ROS_INFO_STREAM("Desconocido");
+		}
+
+		array.data.clear();
+
+		for (int i = 0; i < 8; i++){
+			array.data.push_back(estado[i]);
+		}
+
+		pubEst.publish(array);
+		line_left=false;
+		line_right=false;
+		line_center=false;
 		ros::spinOnce();
 		// Espera hasta que es tiempo de la siguiente iteracion
 		rate.sleep();
-	}	
+	}
+}	
 	
-}
+
